b84cd6c7cfc2964ccd1be30e6f3fe6e539904a26
jorgepim <109249187+jorgepim@users.noreply.github.com>
2023-05-04 21:51:33 -0600

    Add files via upload

---
+++ Candidato.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.CLases
+{
+    internal class Candidato : Persona
+    {
+        //Variables
+        private string partido;
+        private int votos;
+        private decimal inversion;
+        public string memelin;
+        //Setters and Getters
+        public string Partido
+        {
+            get { return partido; }
+            set { partido = value; }
+        }
+        public int Votos
+        {
+            get { return votos; }
+            set { votos = value; }
+        }
+        public decimal Inversion
+        {
+            get { return inversion; }
+            set { inversion = value; }
+        }
+        //Constructor
+        public Candidato(string partido, int votos, decimal inversion, string nombre, int edad, string dui, string direccion, string tel) : base(nombre, edad, dui, direccion, tel)
+        {
+            Partido = partido;
+            Votos = votos;
+            Inversion = inversion;
+        }
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.CLases
+{
+    internal class Persona
+    {
+        //Variahles
+        protected string nombre, dui, direccion, tel;
+        protected int edad;
+        //Setters and Getters
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Dui
+        {
+            get { return dui; }
+            set { dui = value; }
+        }
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        public string Tel
+        {
+            get { return tel; }
+            set { tel = value; }
+        }
+        //Constructor
+        public Persona(string nombre, int edad, string dui, string direccion, string tel)
+        {
+            Nombre = nombre;
+            Dui = dui;
+            Direccion = direccion;
+            Tel = tel;
+            Edad = edad;
+        }
+    }
+}
